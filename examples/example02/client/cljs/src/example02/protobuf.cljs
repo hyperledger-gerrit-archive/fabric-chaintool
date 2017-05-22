413b399743c9d399e0a685372cc16deeba9dde17
Greg Haskins <gregory.haskins@gmail.com>
2017-05-22 11:48:50 -0400

    [FAB-4011] Update clojurescript example
    Our example has been updated to work with 1.0.0-alpha2
    
    Change-Id: Ie350ac444c668f236d375ba885f5abecc7d7042a
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>

---
+++ examples/example02/client/cljs/src/example02/protobuf.cljs
@@ -0,0 +1,26 @@
+(ns example02.protobuf
+  (:require [cljs.nodejs :as nodejs])
+  (:refer-clojure :exclude [get]))
+
+(def path (nodejs/require "path"))
+(def pb (nodejs/require "protobufjs"))
+
+(def builder (.newBuilder pb))
+
+(defn- load [dir name]
+  (let [path (.resolve path dir (str name ".proto"))]
+    (.loadProtoFile pb path builder)
+    (.build builder name)))
+
+(def protos (atom {}))
+
+(defn- get [name]
+  (@protos name))
+
+(defn init [dir interfaces]
+  (let [entries (->> interfaces
+                     (map #(vector % (load dir %)))
+                     (into {}))]
+    (swap! protos merge entries)))
+
+
