938cb4f67c5e1912ca38fc4a1f095ae6d0623f4e
Matt Rkiouak <mrkiouak@gmail.com>
2017-06-30 18:12:32 -0400

    [FAB-5138] Adds support for map types
    protobuf/generate case for :map with nested types in AST
    parsing support for map<type, type> in .cci files via changes to build/interface
    add map test to chaintool/build/test_interface
    add map grammar to resources/interface/grammar
    add map grammar to resources/protobuf/grammar
    add map-test to chaintool/protobuf/test_generate
    
    GMH: Fix whitespace damage and update subject
    
    MR: Patch
    	-build/interface remove dead code in getattrs
    	-protobuf/generate style/readability improvement in typeconvert
    Fix Issue # FAB-5138
    
    Change-Id: I2ead2f794d8aaec5e267f2694a8a5dac4bdcf129
    Signed-off-by: Matt Rkiouak <mrkiouak@gmail.com>
    Signed-off-by: Greg Haskins <gregory.haskins@gmail.com>
    Signed-off-by: Matt Rkiouak <rkiouak@me.com>

--- src/chaintool/build/interface.clj
+++ src/chaintool/build/interface.clj
@@ -64,7 +64,8 @@
 
 (defn getattrs [ast]
   (let [{:keys [type] :as attrs} (get-raw-attrs ast)
-        [subType typeName] type]
+        subType (first type)
+        typeName (if (not= subType :map) (second type) :test)]
     ;; sythesize the subType/typeName fields
     (merge attrs {:subType subType :typeName typeName})))
 
@@ -213,7 +214,8 @@
   (let [{:keys [subType]} (getattrs ast)]
     (case subType
       :userType (verify-usertype ast)
-      :scalar [ast nil])))
+      :scalar [ast nil]
+      :map [ast nil])))
 
 ;;-----------------------------------------------------------------
 ;; check-index-default: ensures our default index is 0
